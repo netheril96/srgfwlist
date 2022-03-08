4c75ffbecbbb8f4d7828b961629702402c12674c
Siyuan Ren <netheril96@gmail.com>
2022-03-09 00:35:00 +0800

    Fix shell ordering.

--- autorebuild.sh
+++ autorebuild.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e;
 set -x;
-git pull --rebase;
 cd "$(dirname "$(realpath "$0")")";
+git pull --rebase;
 python3 ./gensrrules.py;
 git add fullrules.*;
 git commit -m 'Rebuild full rules';
