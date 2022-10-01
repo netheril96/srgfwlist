896b94574dccbf772acadef84f90150d8aae57bb
Autobuilder <autobuilder@auto.builder>
2022-10-01 08:52:06 +0000

    Only commit when changes are made

--- autorebuild.sh
+++ autorebuild.sh
@@ -4,6 +4,10 @@ set -x;
 cd "$(dirname "$(realpath "$0")")";
 git pull --rebase;
 python3 ./gensrrules.py;
-git add fullrules.*;
-git commit -m 'Rebuild full rules';
-git push;
+STATUS_MSG=$(git status);
+if [[ $STATUS_MSG == *'Changes not staged for commit'* ]]
+then
+  git add fullrules.*;
+  git commit -m 'Rebuild full rules';
+  git push;
+fi
